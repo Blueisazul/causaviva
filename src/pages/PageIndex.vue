1be0b648316df8ec3612984c7ef2a6279b9cbc48
Blueisazul <sant.14.go@outlook.com>
2025-04-29 19:27:49 -0500

    visto

--- src/pages/PageIndex.vue
+++ src/pages/PageIndex.vue
@@ -8,15 +8,9 @@ import SectionOne from '../components/SectionOne.vue';
 <template>
   <NavbarSection />
   <SectionOne />
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="h-24 p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo"/>
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="h-24 p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#42b883aa]" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  
+  <HelloWorld />
   <HelloTailwind />
 </template>
 
