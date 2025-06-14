d4a2da00f17c3c44fd6f97cf202f044acce86b3f
Luis Miguel Santiago Caraza <N00214419@upn.pe>
2025-06-14 07:21:06 -0500

    syt

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.css';
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Componentes from './primevue';
